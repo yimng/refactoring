99a67b6905a634617343b357bb0964418b9aa6e3
Lukun <lukun@sinosoft.com.cn>
2020-03-06 12:01:54 +0800

    搬移'金额计算'代码

--- src/main/java/Rental.java
+++ src/main/java/Rental.java
@@ -24,4 +24,28 @@ public class Rental
         this.movie = movie;
         this.daysRented = daysRented;
     }
+
+    public double getCharge() {
+        double result = 0;
+        switch (this.getMovie().getPriceCode()) {
+            case Movie.REGULAR:
+                result += 2;
+                if (this.getDaysRented() > 2) {
+                    result += (this.getDaysRented() - 2) * 1.5;
+                }
+                break;
+            case Movie.NEW_RELEASE:
+                result += this.getDaysRented() * 3;
+                break;
+            case Movie.CHILEDRENS:
+                result += 1.5;
+                if (this.getDaysRented() > 3) {
+                    result += (this.getDaysRented() - 3) * 1.5;
+
+                }
+                break;
+
+        }
+        return result;
+    }
 }
